233b7040a8ba73a337cfefe7b8efa0b21f4c2d3d
Dragica Dolovska <nikodola@gmail.com>
2024-06-25 15:29:32 +0200

    deploy13

--- src/db/index.tsx
+++ src/db/index.tsx
@@ -6,6 +6,9 @@ if (process.env.NODE_ENV !== "production") {
   dotenv.config({ path: ".env.local" });
 }
 
+// Log the DATABASE_URL
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
 // Initialize PrismaClient with the DATABASE_URL
 const prisma = new PrismaClient({
   datasources: {
